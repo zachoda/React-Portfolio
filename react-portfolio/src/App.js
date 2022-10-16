9e0da475270e6c88c2de6c7dfe795d4c88561f04
Zach Oda <zjodachow@gmail.com>
2022-10-16 10:48:45 -0400

    folders updated

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from '../src/components/Navigation';
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
+import Project from '../src/components/Project';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
